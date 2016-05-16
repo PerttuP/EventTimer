b9275a7ca7211fec9696dafc0f081c5ba44457ab
PerttuP <perttu.paarlahti@gmail.com>
2016-05-16 03:22:34 +0300

    Finished DatabaseHandler benchmarking. Results added to the doc-directory. Class diagram added to the doc-directory.

--- UnitTests/DatabaseHandlerBenchmark/tst_databasehandlerbenchmark.cc
+++ UnitTests/DatabaseHandlerBenchmark/tst_databasehandlerbenchmark.cc
@@ -20,8 +20,6 @@ class DatabaseHandlerBenchmark : public QObject
 public:
     DatabaseHandlerBenchmark();
 
-    ~DatabaseHandlerBenchmark();
-
 private Q_SLOTS:
 
     /**
@@ -55,21 +53,65 @@ private Q_SLOTS:
     void addUpdateRemoveSingleEvent_data();
 
 
+    // Big data tests (1000 events):
+    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Benchmark adding 1000 events into empty database.
+     */
+    void addThousandEvents();
+    void addThousandEvents_data();
+
+    /**
+     * @brief Benchmark updating 1000 different events in the database.
+     */
+    void updateThousandEvents();
+    void updateThousandEvents_data();
+
+    /**
+     * @brief Benchmark getting 1000 different events from the database.
+     */
+    void getThousandEvents();
+    void getThousandEvents_data();
+
+    /**
+     * @brief Benchmark getting 500 expired events from the database.
+     */
+    void get500ExpiredEvents();
+    void get500ExpiredEvents_data();
+
+    /**
+     * @brief Benchmark clearing 500 dynamic events.
+     */
+    void clear500DynamicEvents();
+    void clear500DynamicEvents_data();
+
+    /**
+     * @brief Benchmark clearing remaining 500 events.
+     */
+    void clear500Events();
+    void clear500Events_data();
+
+    /**
+     * @brief Benchmark removing 1000 events from the database one by one.
+     */
+    void removeThousandEvents();
+    void removeThousandEvents_data();
+
+
 private:
 
     std::shared_ptr<EventTimerNS::DatabaseHandler>
     initDB(QString dbType, QString dbName, QString tableName, QString dbHost, QString userName, QString password);
+
+    // Container for storing Events in big data tests.
+    std::vector<EventTimerNS::Event> events_;
+    QDateTime currentTime_;
 };
 
 
 DatabaseHandlerBenchmark::DatabaseHandlerBenchmark()
 {   
-    qDebug() <<"Ctor";
-}
-
-DatabaseHandlerBenchmark::~DatabaseHandlerBenchmark()
-{
-    qDebug() << "Dtor";
 }
 
 
@@ -124,6 +166,7 @@ void DatabaseHandlerBenchmark::addEventBenchmark()
     using namespace EventTimerNS;
     std::shared_ptr<DatabaseHandler> handler =
             this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+    handler->clearAll();
 
     QBENCHMARK {
         Event e("eventName", "2000-01-01 00:00:00:000", Event::STATIC, 1000, 123);
@@ -152,6 +195,7 @@ void DatabaseHandlerBenchmark::updateSingleBenchmark()
     using namespace EventTimerNS;
     std::shared_ptr<DatabaseHandler> handler =
             this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+    handler->clearAll();
 
     Event original("original", "2000-01-01 00:00:00:000", Event::DYNAMIC, 0, 0);
     Event updated("updated", "2016-05-13 17:11:00:000", Event::DYNAMIC, 10, 10);
@@ -183,6 +227,7 @@ void DatabaseHandlerBenchmark::getEventSingle()
     using namespace EventTimerNS;
     std::shared_ptr<DatabaseHandler> handler =
             this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+    handler->clearAll();
 
     Event original("original", "2000-01-01 00:00:00:000", Event::DYNAMIC, 0, 0);
     handler->addEvent(&original);
@@ -214,6 +259,7 @@ void DatabaseHandlerBenchmark::addUpdateRemoveSingleEvent()
     using namespace EventTimerNS;
     std::shared_ptr<DatabaseHandler> handler =
             this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+    handler->clearAll();
 
     QBENCHMARK {
         Event original("original", "2000-01-01 00:00:00:000", Event::DYNAMIC, 0, 0);
@@ -233,6 +279,285 @@ void DatabaseHandlerBenchmark::addUpdateRemoveSingleEvent_data()
 }
 
 
+void DatabaseHandlerBenchmark::addThousandEvents()
+{
+    QFETCH(QString, dbType);
+    QFETCH(QString, dbName);
+    QFETCH(QString, tableName);
+    QFETCH(QString, dbHost);
+    QFETCH(QString, userName);
+    QFETCH(QString, password);
+
+    using namespace EventTimerNS;
+    std::shared_ptr<DatabaseHandler> handler =
+            this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+    handler->clearAll();
+    events_.clear();
+
+    // Create events.
+    currentTime_ = QDateTime::currentDateTime();
+    for (int i=1; i<=1000; ++i){
+        Event e;
+        if (i%2 == 0){
+            // Even id = Static expired.
+            e = Event("name" + QString::number(i), currentTime_.addDays(-i).toString(Event::TIME_FORMAT),
+                      Event::STATIC, i*1000, Event::INFINITE_REPEAT);
+        }
+        else {
+            // Odd id = Dynamic future.
+            e = Event("name" + QString::number(i), currentTime_.addDays(i).toString(Event::TIME_FORMAT),
+                      Event::DYNAMIC, i*1000, Event::INFINITE_REPEAT);
+        }
+        events_.push_back(e);
+    }
+    QVERIFY(events_.size() == 1000);
+
+    // Add events
+    QBENCHMARK_ONCE {
+        for (unsigned i=0; i<events_.size(); ++i) {
+            handler->addEvent(&(events_[i]));
+        }
+    }
+
+    // Check that events were added
+    for (Event e : events_){
+        QVERIFY(e.id() != -1);
+    }
+}
+
+
+void DatabaseHandlerBenchmark::addThousandEvents_data()
+{
+    constructorBenchmark_data();
+}
+
+
+void DatabaseHandlerBenchmark::updateThousandEvents()
+{
+    QFETCH(QString, dbType);
+    QFETCH(QString, dbName);
+    QFETCH(QString, tableName);
+    QFETCH(QString, dbHost);
+    QFETCH(QString, userName);
+    QFETCH(QString, password);
+    QVERIFY(events_.size() == 1000);
+
+    using namespace EventTimerNS;
+    std::shared_ptr<DatabaseHandler> handler =
+            this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+
+    // Create updated events.
+    std::vector<Event> updated;
+    for (Event e : events_){
+        Event u;
+        if (e.id() % 2 == 0){
+            u = Event(e.name()+"_u",
+                      QDateTime::fromString(e.timestamp(), Event::TIME_FORMAT).addDays(-1).toString(Event::TIME_FORMAT),
+                      Event::DYNAMIC, e.interval()+1000, e.repeats()+1);
+        }
+        else {
+            u = Event(e.name()+"_u",
+                      QDateTime::fromString(e.timestamp(), Event::TIME_FORMAT).addDays(1).toString(Event::TIME_FORMAT),
+                      Event::STATIC, e.interval()+1000, e.repeats()+1);
+        }
+        updated.push_back(u);
+    }
+    QVERIFY(updated.size() == 1000);
+
+    // Update events
+    QBENCHMARK_ONCE {
+        for (unsigned i=0; i<events_.size(); ++i){
+            QVERIFY(handler->updateEvent(events_[i].id(), updated[i]));
+        }
+    }
+}
+
+
+void DatabaseHandlerBenchmark::updateThousandEvents_data()
+{
+    constructorBenchmark_data();
+}
+
+
+void DatabaseHandlerBenchmark::getThousandEvents()
+{
+    QFETCH(QString, dbType);
+    QFETCH(QString, dbName);
+    QFETCH(QString, tableName);
+    QFETCH(QString, dbHost);
+    QFETCH(QString, userName);
+    QFETCH(QString, password);
+    QVERIFY(events_.size() == 1000);
+
+    using namespace EventTimerNS;
+    std::shared_ptr<DatabaseHandler> handler =
+            this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+
+    // Update events_ -container.
+    QBENCHMARK_ONCE {
+        for (unsigned i=0; i<events_.size(); ++i){
+            events_[i] = handler->getEvent(events_[i].id());
+        }
+    }
+
+    // Verify that all events are right.
+    for (Event e : events_) {
+        QVERIFY(e.id() != -1);
+    }
+}
+
+
+void DatabaseHandlerBenchmark::getThousandEvents_data()
+{
+    constructorBenchmark_data();
+}
+
+
+void DatabaseHandlerBenchmark::get500ExpiredEvents()
+{
+    QFETCH(QString, dbType);
+    QFETCH(QString, dbName);
+    QFETCH(QString, tableName);
+    QFETCH(QString, dbHost);
+    QFETCH(QString, userName);
+    QFETCH(QString, password);
+    QVERIFY(events_.size() == 1000);
+
+    using namespace EventTimerNS;
+    std::shared_ptr<DatabaseHandler> handler =
+            this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+
+    // Get expired
+    std::vector<Event> expired;
+    QBENCHMARK_ONCE{
+        expired = handler->checkOccured(currentTime_);
+    }
+
+    // Check that expired events are right.
+    QCOMPARE(expired.size(), std::vector<Event>::size_type(500));
+    for (Event e : expired){
+        QVERIFY(e.id() != -1);
+        QVERIFY(e.timestamp() < currentTime_.toString(Event::TIME_FORMAT));
+        Event original = events_[e.id()-1];
+        QCOMPARE(e.id(), original.id());
+        QCOMPARE(e.name(), original.name());
+        QCOMPARE(e.timestamp(), original.timestamp());
+        QCOMPARE(e.type(), original.type());
+        QCOMPARE(e.interval(), original.interval());
+        QCOMPARE(e.repeats(), original.repeats());
+    }
+}
+
+
+void DatabaseHandlerBenchmark::get500ExpiredEvents_data()
+{
+    constructorBenchmark_data();
+}
+
+
+void DatabaseHandlerBenchmark::clear500DynamicEvents()
+{
+    QFETCH(QString, dbType);
+    QFETCH(QString, dbName);
+    QFETCH(QString, tableName);
+    QFETCH(QString, dbHost);
+    QFETCH(QString, userName);
+    QFETCH(QString, password);
+    QVERIFY(events_.size() == 1000);
+
+    using namespace EventTimerNS;
+    std::shared_ptr<DatabaseHandler> handler =
+            this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+
+    QBENCHMARK_ONCE {
+        QVERIFY(handler->clearDynamic());
+    }
+
+    // Verify that dynamic events were removed
+    for (Event e : events_) {
+        if (e.type() == Event::DYNAMIC){
+            QCOMPARE(handler->getEvent(e.id()).id(), -1);
+        }
+        else {
+            QCOMPARE(handler->getEvent(e.id()).id(), e.id());
+        }
+    }
+}
+
+
+void DatabaseHandlerBenchmark::clear500DynamicEvents_data()
+{
+    constructorBenchmark_data();
+}
+
+
+void DatabaseHandlerBenchmark::clear500Events()
+{
+    QFETCH(QString, dbType);
+    QFETCH(QString, dbName);
+    QFETCH(QString, tableName);
+    QFETCH(QString, dbHost);
+    QFETCH(QString, userName);
+    QFETCH(QString, password);
+    QVERIFY(events_.size() == 1000);
+
+    using namespace EventTimerNS;
+    std::shared_ptr<DatabaseHandler> handler =
+            this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+
+    QBENCHMARK_ONCE {
+        QVERIFY(handler->clearAll());
+    }
+
+    // Verify that all events are removed
+    for (Event e : events_) {
+        QCOMPARE(handler->getEvent(e.id()).id(), -1);
+    }
+}
+
+
+void DatabaseHandlerBenchmark::clear500Events_data()
+{
+    constructorBenchmark_data();
+}
+
+
+void DatabaseHandlerBenchmark::removeThousandEvents()
+{
+    QFETCH(QString, dbType);
+    QFETCH(QString, dbName);
+    QFETCH(QString, tableName);
+    QFETCH(QString, dbHost);
+    QFETCH(QString, userName);
+    QFETCH(QString, password);
+    QVERIFY(events_.size() == 1000);
+
+    using namespace EventTimerNS;
+    std::shared_ptr<DatabaseHandler> handler =
+            this->initDB(dbType, dbName, tableName, dbHost, userName, password);
+
+    // Re-populate database.
+    for (unsigned i=0; i<events_.size(); ++i){
+        QVERIFY(handler->getEvent(events_[i].id()).id() == -1);
+        // Note: violates precondition. Works with release build only.
+        // Violation has no undesired side effects in this case.
+        QVERIFY(handler->addEvent(&(events_[i])) != -1);
+    }
+
+    QBENCHMARK_ONCE {
+        for (Event e : events_){
+            QVERIFY(handler->removeEvent(e.id()));
+        }
+    }
+}
+
+
+void DatabaseHandlerBenchmark::removeThousandEvents_data()
+{
+    constructorBenchmark_data();
+}
+
+
 std::shared_ptr<EventTimerNS::DatabaseHandler>
 DatabaseHandlerBenchmark::initDB(QString dbType, QString dbName, QString tableName, QString dbHost, QString userName, QString password)
 {
@@ -245,7 +570,6 @@ DatabaseHandlerBenchmark::initDB(QString dbType, QString dbName, QString tableNa
     setup.password = password;
 
     std::shared_ptr<EventTimerNS::DatabaseHandler> h(new EventTimerNS::DatabaseHandler(setup));
-    h->clearAll();
     return h;
 }
 
